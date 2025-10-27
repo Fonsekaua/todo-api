b45fd90a7b11df3324ccc506c640bbc108022a74
= <fonsecakauasilva3@gmail.com>
2025-10-27 18:52:10 -0500

    Servidor node basico, para estudos

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,10 +1,16 @@
-import express from 'express'
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const server = express();
+const PORT = process.env.PORT;
 
 server.use('/',(req,res) => {
     res.send("Modulo: Integrando node com banco de dados")
-})
+});
 
-server.listen(3333)
+server.listen(PORT,() => {
+    console.log(`Servidor rodando na porta http://localhost:${PORT}`)
+});
 
