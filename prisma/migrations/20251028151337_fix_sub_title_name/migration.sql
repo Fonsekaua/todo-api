23a7933b2677c2d4dc392ac9083da5dffd82cc59
fonsekaua <fonsecakauasilva3@gmail.com>
2025-10-28 11:47:49 -0500

    att de dados necessarios

--- prisma/migrations/20251028151337_fix_sub_title_name/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `sub_title` on the `posts` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `posts` DROP COLUMN `sub_title`,
-    ADD COLUMN `subTitle` VARCHAR(191) NULL;
